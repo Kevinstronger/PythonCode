1a2743da360ce0fcf3b1afff0796772af12e7bf6
Kevinstronger <zhang1830@gmail.com>
2014-01-28 17:49:39 +0800

    Raw_input

---
+++ ex11.py
@@ -0,0 +1,19 @@
+#coding=UTF-8
+
+print "你几岁啦？",
+age = raw_input()
+print "你有多高呀？",
+height = raw_input()
+print "你有多重呀？",
+weight = raw_input()
+
+print "你现在 %r 岁， %r 厘米高，%r 公斤重。" %(age, height, weight)
+
+'''
+你几岁啦？ 5
+你有多高呀？ 124
+你有多重呀？ 36
+你现在 '5' 岁， '124' 厘米高，'36' 公斤重。
+
+'''
+
