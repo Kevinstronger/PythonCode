86451466b669a8c34f2a612ed26d52218460db66
Kevinstronger <zhang1830@gmail.com>
2014-01-28 17:24:11 +0800

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -12,3 +12,17 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+'''
+Result:
+    I'm tabbed in.
+I'm split
+on a line.
+I'm \ a \ cat.
+
+I'll do a list:
+    * Cat food
+    * Fishies
+    * Catnip
+    * Grass
+'''
