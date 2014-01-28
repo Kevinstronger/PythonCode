f1833c826a568443d8c9caecabeb442ba9791adc
Kevinstronger <zhang1830@gmail.com>
2014-01-28 18:00:00 +0800

    Prompting

---
+++ ex12.py
@@ -0,0 +1,14 @@
+#coding=UTF-8
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight")
+
+print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
+
+'''
+How old are you?5
+How tall are you?6'2"
+How much do you weight1081bs
+So, you're '5' old, '6\'2"' tall and '1081bs' heavy.
+'''
