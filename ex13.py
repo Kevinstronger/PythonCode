7132caf7dcb4c61d9ecdd9271505a531678ae396
Kevinstronger <zhang1830@gmail.com>
2014-02-10 10:31:23 +0800

    Parameters\Unpacking\Variables

---
+++ ex13.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+'''
+The script is called: D:\PY_Workspace\FirstPro\src\training\ex13.py
+Your first variable is: a,
+Your second variable is: b,
+Your third variable is: c
+
+'''
